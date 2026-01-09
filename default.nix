714f141fc6105151e549b3fea1c739336e474b53
Gaurav Juvekar <gauravjuvekar@gmail.com>
2026-01-09 08:41:58 +0530

    add slashed variant

--- default.nix
+++ default.nix
@@ -1,9 +1,9 @@
-{ pkgs }:
+slug: { pkgs }:
 pkgs.stdenv.mkDerivation {
-  name = "droid-sans-mono-dotted-nerd-font";
+  name = "droid-sans-mono-${pkgs.lib.strings.toLower slug}-nerd-font";
   srcs =
     [
-      ./DroidSansMonoDotted.ttf
+      ./DroidSansMono${slug}.ttf
     ];
   nativeBuildInputs = [ pkgs.nerd-font-patcher ];
 
@@ -18,13 +18,13 @@ pkgs.stdenv.mkDerivation {
   buildPhase = ''
     runHook preBuild
 
-    nerd-font-patcher -c --no-progressbars ./DroidSansMonoDotted.ttf
+    nerd-font-patcher -c --no-progressbars ./DroidSansMono${slug}.ttf
 
     runHook postBuild
   '';
   installPhase = ''
     runHook preInstall
-    install -m444 -Dt $out/share/fonts/truetype ./DroidSansMDottedNerdFont-Regular.ttf
+    install -m444 -Dt $out/share/fonts/truetype ./DroidSansM${slug}NerdFont-Regular.ttf
 
     runHook postInstall
   '';
--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "DroidSans Mono Dotted 0 Nerd Font";
+  description = "DroidSans Mono Dotted and Slashed Nerd Font";
 
   inputs =
     {
@@ -12,12 +12,13 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
         droid-sans-func = import ./default.nix;
-        droid-sans-package = (droid-sans-func {pkgs=pkgs;});
+        droid-sans-dotted-package = (droid-sans-func "Dotted" {pkgs=pkgs;});
+        droid-sans-slashed-package = (droid-sans-func "Slashed" {pkgs=pkgs;});
       in
         {
           packages = rec {
-            droid-sans-mono-dotted-nerdfont = droid-sans-package;
-            default = droid-sans-mono-dotted-nerdfont;
+            droid-sans-mono-dotted-nerdfont = droid-sans-dotted-package;
+            droid-sans-mono-slashed-nerdfont = droid-sans-slashed-package;
           };
         }
     );
